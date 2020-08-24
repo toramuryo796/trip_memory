ca6c81126d10eb8c41dd599fa95288153288b169
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-25 02:30:14 +0900

    グループデータ取得

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,14 +5,19 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    binding.pry
+    group_params
   end
-
+  
   def show
   end
-
-
-
+  
+  private
+  def group_params
+    params.require(:group).permit(:commit, :name, members:[]).merge(user_id: current_user.id)
+  end
 end
