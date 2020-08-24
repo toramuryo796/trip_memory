c0101e4bdf534b090147bd3f2133265bef965f61
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-25 02:28:21 +0900

    memberカラム追加

--- db/migrate/20200821143540_create_groups.rb
+++ db/migrate/20200821143540_create_groups.rb
@@ -2,6 +2,7 @@ class CreateGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :groups do |t|
       t.string :name,      null: false
+      t.string :member,    null: false
       t.timestamps
     end
   end
