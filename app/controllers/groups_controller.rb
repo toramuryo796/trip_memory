00754ca04140188f55aa08c858e328383e6cf139
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-25 10:10:52 +0900

    グループ作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,8 +9,24 @@ class GroupsController < ApplicationController
   end
 
   def create
-    binding.pry
-    group_params
+    @group = Group.create(name: group_params[:name])
+    members = group_params[:members]
+    user_ids = []
+    @user = nil
+    @user_group = nil 
+    count = 0
+    members.each do |member|
+      @user = User.find_by(nickname: member)
+      @user_group = UserGroup.new(group_id: @group.id, user_id: @user.id) 
+      if @user_group.save
+        count += 1
+      end
+    end
+    if count == members.length
+      redirect_to group_path(@group)
+    else
+      redirect_to :new
+    end
   end
   
   def show
