0f96ed8117a3f6894f0f501e56f8d5d20d63063a
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-25 10:10:14 +0900

    マイグレーションファイル修正

--- db/migrate/20200821143540_create_groups.rb
+++ db/migrate/20200821143540_create_groups.rb
@@ -2,7 +2,6 @@ class CreateGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :groups do |t|
       t.string :name,      null: false
-      t.string :member,    null: false
       t.timestamps
     end
   end
