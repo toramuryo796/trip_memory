aac14c7c9554f74c0b6901f8c895a5478b57c2a6
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-09-05 01:20:11 +0900

    スケジュール 1日目 ajax　完

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,41 +1,73 @@
 class SchedulesController < ApplicationController
-  def new
-    @schedule = Schedule.new
-  end
+  before_action :find_schedule, only: [:edit, :destroy]
 
+  def new
+  end
+  
   def index
     @plan = Plan.find(params[:format])
     @day = @plan.day_hows
+    @plan_id = @plan.id
     @other_days = DayHow.where(plan_id: @plan.id)
+    @group = @plan.group
+    @schedules = Schedule.where(plan_id: @plan.id)
     @plan.day_hows.each do |day_how| 
       @passed = day_how.passed
+      @day_schedules = day_how.schedules
+      @day_id = day_how.id
     end
-    @group = @plan.group
-    @schedules = @plan.schedules.includes(:group)
   end
   
   def create
-    @schedule = Schedule.create( hour_id: schedule_params[:hour_id], minute_id: schedule_params[:minute_id], program: schedule_params[:program], category_id: schedule_params[:category_id], plan_id: schedule_params[:format], user_id: schedule_params[:user_id])
+    @schedule = Schedule.create( 
+      hour_id: schedule_params[:hour_id],
+      minute_id: schedule_params[:minute_id],
+      program: schedule_params[:program], 
+      category_id: schedule_params[:category_id], 
+      plan_id: schedule_params[:format], 
+      user_id: schedule_params[:user_id], 
+      day_how_id: schedule_params[:day_id]
+    )
+  end
+  
+  def edit
+    plan = @schedule.plan
+    @plan_id = plan.id
   end
   
   def update
+    @schedule = Schedule.find_by(id: params[:id]) 
+    @schedule.update(id: schedule_params[:id], 
+      hour_id: schedule_params[:hour_id],
+      minute_id: schedule_params[:minute_id],
+      program: schedule_params[:program],
+      category_id: schedule_params[:category_id],
+      plan_id: schedule_params[:format],
+      user_id: schedule_params[:user_id],
+      day_how_id: schedule_params[:day_how_id]
+    )
   end
   
-  def delete
+  def destroy
+    @schedule.destroy
   end
   
   private
-
+  
   def schedule_params
-    params.require(:schedule).permit(:hour_id, :minute_id, :program, :category_id, :format).merge(user_id: current_user.id)
+    params.require(:schedule).permit(:hour_id, :minute_id, :program, :category_id, :format, :day_id).merge(user_id: current_user.id)
     #formatはPlanのid
   end
-
+  
   def move_to_new
     plan = Plan.find_by(plan_id: params[:plan_id])
     @schedules = plan.schedules.includes(:group)
     unless @schedules
       render :new
     end 
+  end
+  
+  def find_schedule
+    @schedule = Schedule.find(params[:id])    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'candidates/new'
   devise_for :users
   root "groups#index"
-  resources :schedules,     only: [:index, :create, :show, :update, :delete]
+  resources :schedules,     only: [:index, :new, :create, :update, :destroy, :edit]
   resources :day_hows,      only: [:index, :new, :create, :show, :update, :destroy]
 
   resources :groups,       only: [:index, :new, :create, :show, :destroy] do
