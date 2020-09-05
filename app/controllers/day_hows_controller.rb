5de5171cb1e41e57b283efa0ad1fe8496f551767
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-09-05 18:37:31 +0900

    ajax スケジュール修正

--- app/controllers/day_hows_controller.rb
+++ app/controllers/day_hows_controller.rb
@@ -6,32 +6,42 @@ class DayHowsController < ApplicationController
 
   def new
     @day_how = DayHow.new
-    @plan = Plan.find_by(id: params[:format])         #formatにはplanのidが入っている
+    @plan = Plan.find_by(id: day_how_params[:plan_id])                                        #formatにはplanのidが入っている
+    @plan_id = @plan.id
   end
 
   def create
-    @plan = Plan.find_by(id: create_day_how[:format])
+    plan = Plan.find_by(id: create_day_how[:plan_id])
     day_how = DayHow.new(passed: create_day_how[:passed],
-      plan_id: create_day_how[:format],
+      plan_id: create_day_how[:plan_id],
       user_id: create_day_how[:user_id]
     )
-    if day_how != @plan.day_hows                # すでに取得している日程じゃないか確認
+    exsist_day_hows = DayHow.where(plan_id: plan.id).select(:plan_id, :passed).distinct
+    count = 0
+    # すでに取得している日程じゃないか確認して保存
+    exsist_day_hows.each do |day|
+      if day.passed == day_how.passed                                                           # すでにある日にちだとcountが加算される      
+        count += 1
+      end
+    end
+    if count < 1                                                                                 #countに加算されていれば、同じ日付があるということなので、保存できない
       if day_how.save
-        redirect_to schedules_path(@plan.id)
+        binding.pry
+        redirect_to schedules_path(day_how_id: day_how.id, plan_id: plan.id)
       else
-        render :new
+        redirect_to new_day_how_path(plan_id: plan.id)
       end
     else
-      render :new
+      redirect_to new_day_how_path(plan_id: plan.id)
     end
   end
 
   private
   def day_how_params
-    params.permit(:passed, :format).merge(user_id: current_user.id)
+    params.permit(:passed, :plan_id).merge(user_id: current_user.id)
   end
   
   def create_day_how
-    params.require(:day_how).permit(:passed, :format).merge(user_id: current_user.id)
+    params.require(:day_how).permit(:passed, :plan_id).merge(user_id: current_user.id)
   end
 end
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -5,28 +5,21 @@ class SchedulesController < ApplicationController
   end
   
   def index
-    @plan = Plan.find(params[:format])                 # 計画を特定
-    @plan_id = @plan.id                                # plan_idを取得
-    @other_days = DayHow.where(plan_id: @plan.id)      # palnのday_howを特定
-    @group = @plan.group                               # planのグループを特定
-    @schedules = Schedule.where(plan_id: @plan.id)     # planのスケジュールを特定
-    @other_days.each do |day_how|                      # 日にち毎の各カラム値を取得する
-      @passed = day_how.passed
-      @day_schedules = day_how.schedules
-      @day_id = day_how.id
-    end
+      @plan = Plan.find_by(id: first_params[:plan_id])
+      @plan_id = @plan.id                                # plan_idを取得
+      @schedules = Schedule.where(plan_id: @plan_id)  # 日程に該当するのスケジュールを特定
+      @other_days = DayHow.where(plan_id: @plan.id).select(:plan_id, :passed).distinct.order("passed ASC")        # palnのday_howを特定
+      @other_days.each do |day_how|                        # 日にち毎の各カラム値を取得する
+        @passed = day_how.passed
+        @day_schedules = day_how.schedules
+        @day_id = day_how.id
+      end
+    @group = @plan.group                                 # planのグループを特定
+    # @schedules = Schedule.where(plan_id: plan_id )     # planのスケジュールを特定
   end
   
   def create
-    @schedule = Schedule.create(                       # paln画面からplan_idをformatで送ってもらっているため←の書き方
-      hour_id: schedule_params[:hour_id],
-      minute_id: schedule_params[:minute_id],
-      program: schedule_params[:program], 
-      category_id: schedule_params[:category_id], 
-      plan_id: schedule_params[:format],               #
-      user_id: schedule_params[:user_id], 
-      day_how_id: schedule_params[:day_id]
-    )
+    @schedule = Schedule.create(schedule_params)          
   end
   
   def edit
@@ -36,17 +29,10 @@ class SchedulesController < ApplicationController
   
   def update
     @schedule = Schedule.find_by(id: params[:id])       #formatをしようしているため、←の書き方
-    @schedule.update(id: schedule_params[:id], 
-      hour_id: schedule_params[:hour_id],
-      minute_id: schedule_params[:minute_id],
-      program: schedule_params[:program],
-      category_id: schedule_params[:category_id],
-      plan_id: schedule_params[:format],
-      user_id: schedule_params[:user_id],
-      day_how_id: schedule_params[:day_how_id]
-    )
+    @schedule.update(schedule_params)
     plan = @schedule.plan                              
     @plan_id = plan.id
+    binding.pry
   end
   
   def destroy
@@ -56,9 +42,14 @@ class SchedulesController < ApplicationController
   private
   
   def schedule_params
-    params.require(:schedule).permit(:hour_id, :minute_id, :program, :category_id, :format, :day_id).merge(user_id: current_user.id)
+    params.require(:schedule).permit(:hour_id, :minute_id, :program, :category_id, :plan_id, :day_how_id).merge(user_id: current_user.id)
     #formatはPlanのid
   end
+  def first_params
+    params.permit(:hour_id, :minute_id, :program, :category_id, :plan_id, :day_how_id).merge(user_id: current_user.id)
+    #formatはPlanのid
+  end
+  
   
   def find_schedule
     @schedule = Schedule.find(params[:id])    
