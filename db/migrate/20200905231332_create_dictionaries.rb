bf89c8a8e62b832c7dfb8791506fb9e2c9750a44
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-09-09 23:05:26 +0900

    単語帳　修正

--- db/migrate/20200905231332_create_dictionaries.rb
+++ db/migrate/20200905231332_create_dictionaries.rb
@@ -1,9 +1,9 @@
 class CreateDictionaries < ActiveRecord::Migration[6.0]
   def change
     create_table :dictionaries do |t|
-      t.string     :language,         null:false
+      t.string     :language
       t.string     :japanese,         null:false
-      t.string     :translation,      null:false
+      t.string     :translation,      null:false 
       t.references :plan,             null:false, foreign_key: true
       t.references :user,             null:false, foreign_key: true
       t.references :group,            null:false, foreign_key: true
