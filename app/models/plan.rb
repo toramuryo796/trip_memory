1fab5c484b067c60f4cbaedac17ce730bd59c816
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-09-11 02:15:48 +0900

    細かい修正

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,12 +1,12 @@
 class Plan < ApplicationRecord
   belongs_to        :user 
   belongs_to        :group
-  belongs_to        :candidate, optional: true
-  has_many          :schedules, dependent: :destroy
-  has_many          :day_hows, dependent: :destroy
-  has_one_attached  :image
+  belongs_to        :candidate,    optional: true
+  has_many          :schedules,    dependent: :destroy
+  has_many          :day_hows,     dependent: :destroy
+  has_one_attached  :image,        dependent: :destroy
   has_many          :dictionaries, dependent: :destroy
-  has_one           :memory, dependent: :destroy
+  has_one           :memory,       dependent: :destroy
   # has_one :Bring
   # has_one :purpose
   extend ActiveHash::Associations::ActiveRecordExtensions
