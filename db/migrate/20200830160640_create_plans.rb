29c4b3b7425a386e7055fb83633d95c4ab83f32e
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-09-15 23:17:06 +0900

    candidate 単体テスト

--- db/migrate/20200830160640_create_plans.rb
+++ db/migrate/20200830160640_create_plans.rb
@@ -3,13 +3,13 @@ class CreatePlans < ActiveRecord::Migration[6.0]
     create_table :plans do |t|
       t.string     :title,              null: false
       t.string     :destination,        null: false
-      t.date       :departure_day    
-      t.date       :return_day       
-      t.string     :hotel            
-      t.text       :hotel_memo       
+      t.date       :departure_day               
+      t.date       :return_day                   
+      t.string     :hotel                             
+      t.text       :hotel_memo                     
       t.integer    :transportation_id
-      t.string     :start_place
-      t.text       :ticket
+      t.string     :start_place               
+      t.text       :ticket               
       t.references :user,             null:false, foreign_key:true        
       t.references :group,            null:false, foreign_key:true        
       t.references :candidate,        foreign_key:true        
