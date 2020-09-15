f829d1082223baafa248cada3c26211bf4856f67
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-09-15 23:19:38 +0900

    schedule　単体テスト

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :schedule do
-    
-  end
+    hour_id         { 8 }
+    minute_id       { 5 }
+    program         { "aiueoかきくけこサシスセソ123" } 
+    category_id     { 2 } 
+    association :plan
+    association :user
+    association :day_how
+  end         
 end
--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Schedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "スケジュール" do
+    before do
+      @schedule = FactoryBot.build(:schedule)
+    end
+    it "全て正しい情報を入力していれば登録できる" do
+      expect(@schedule).to be_valid
+    end
+
+    it " 予定が101字以上だと登録できない " do
+      @schedule.program = Faker::Lorem.characters(number: 101)
+      @schedule.invalid?
+      expect(@schedule.errors.full_messages).to include("予定は100字以内で記入してください")
+    end
+  end
 end
