250357fd9c5f00381d3fb1c3d49c72c36f37485f
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-09-15 23:14:00 +0900

    goodモデル削除

--- app/models/good.rb
+++
@@ -1,5 +0,0 @@
-class Good < ApplicationRecord
-  belongs_to :candidate
-  belongs_to :user
-  validates_uniqueness_of :candidate_id, scope: :user_id
-end
--- spec/factories/goods.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :good do
-    
-  end
-end
